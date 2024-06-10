26a03ae9478ee3963d8779d2c309dfe251242545
TheXbots <118382544+TheXbots@users.noreply.github.com>
2024-06-09 22:37:27 -0400

    Update OpenSource.lua

--- OpenSource.lua
+++ OpenSource.lua
@@ -43,6 +43,42 @@ function Clipboard(value)
 	end
 end
 
+function API:Notif(Text,Dur)
+	task.spawn(function()
+		if not Dur then
+			Dur = 1.5
+		end
+		local Notif = Instance.new("ScreenGui")
+		local Frame_1 = Instance.new("Frame")
+		local TextLabel = Instance.new("TextLabel")
+		Notif.Parent = (game:GetService("CoreGui") or gethui())
+		Notif.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+		Frame_1.Parent = Notif
+		Frame_1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+		Frame_1.BackgroundTransparency=1
+		Frame_1.BorderSizePixel = 0
+		Frame_1.Position = UDim2.new(0, 0, 0.0500000007, 0)
+		Frame_1.Size = UDim2.new(1, 0, 0.100000001, 0)
+		TextLabel.Parent = Frame_1
+		TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		TextLabel.BackgroundTransparency = 1.000
+		TextLabel.TextTransparency =1
+		TextLabel.Size = UDim2.new(1, 0, 1, 0)
+		TextLabel.Font = Enum.Font.Highway
+		TextLabel.Text = Text or "Text not found"
+		TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+		TextLabel.TextSize = 21.000
+		API:Tween(Frame_1,"BackgroundTransparency",0.350,.5)
+		API:Tween(TextLabel,"TextTransparency",0,.5)
+		wait(Dur+.7)
+		API:Tween(Frame_1,"BackgroundTransparency",1,.5)
+		API:Tween(TextLabel,"TextTransparency",1,.5)
+		wait(.7)
+		Notif:Destroy()
+	end)
+	return
+end
+
 function API:UnSit()
 	game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").Sit = false
 end
