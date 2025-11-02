f237fb69f72b1000d8e0a10e2586ea96b119c009
GEX <118382544+GexAPI@users.noreply.github.com>
2025-11-02 01:15:32 -0500

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -3,5 +3,7 @@
   "10KMisterGold3" : true,
   "DrDoomsDemise" : true,
   "DrDoomEX" : true,
-  "TheGoIdenOne1" : true
+  "TheGoIdenOne1" : true,
+  "TheNathanIsTheBad" : true,
+  "MasonIsH0M0", true
 }
