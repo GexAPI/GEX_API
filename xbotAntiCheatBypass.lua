7a12f782f3dd2907ac088d0b3c3bbd02da2e41ff
GEX <118382544+GexAPI@users.noreply.github.com>
2025-11-03 21:22:37 -0500

    Update xbotAntiCheatBypass.lua

--- xbotAntiCheatBypass.lua
+++ xbotAntiCheatBypass.lua
@@ -1,13 +1,10 @@
-local object = setmetatable({}, { __index = newcclosure(function() return false end), __metatable = "Locked!" })
-local ref = hookmetamethod(object, "__index", function() return true end)
-
-local isMetaMethod = false
-
 local function = test()
-    
+    local object = setmetatable({}, { __index = newcclosure(function() return false end), __metatable = "Locked!" })
+    local ref = hookmetamethod(object, "__index", function() return true end)
+    return assert(object.test == true, "Failed to hook a metamethod and change the return value")    
 end
-assert(object.test == true, "Failed to hook a metamethod and change the return value")
 
+local isMetaMethod = test()
 local originalNameCall
 
 originalNameCall = hookmetamethod(game, "__namecall", function(self,...)
@@ -15,7 +12,9 @@ originalNameCall = hookmetamethod(game, "__namecall", function(self,...)
     if method == "FireServer" and self:IsA("RemoteEvent") or self.Name == "Connect" then
         --intercept the call and give our own 
         local args = {...} -- temporary, but now we've successfully hooked
-        
+        if type(args[31]) == "boolean" then
+            args[31] = false
+        end
         local result = originalNameCall(self, unpack(args))
     else
         return  originalNameCall(self, ...)   
